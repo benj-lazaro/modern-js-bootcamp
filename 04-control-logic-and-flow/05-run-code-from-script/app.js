25df6ab236f06211d40bd28250248cb48aa17045
benj-lazaro <benjie.work@gmail.com>
2023-08-14 23:22:24 +0800

    Section 04 running js code from script

---
+++ 04-control-logic-and-flow/05-run-code-from-script/app.js
@@ -0,0 +1,15 @@
+alert("It's working!");
+
+// The result from this line of code will NOT show up anywhere
+"hi".toUpperCase()
+
+// The following lines of code prints result to the browser's console
+// The .log method of the console basically logs/prints the results to the browser's console window
+console.log("hi".toUpperCase());
+console.log(3 + 4);
+
+// You can pass multiple arguments to the .log() method of the console
+console.log(10 + 10, "Hi there!", true);
+
+// The .error method of the console returns the passed value like a JavaScript error
+console.error("OH MY!!!");
