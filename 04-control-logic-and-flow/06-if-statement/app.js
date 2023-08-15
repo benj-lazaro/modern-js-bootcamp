5b2fcb91e149e5f9fc49e7abb22a773a816eac28
benj-lazaro <benjie.work@gmail.com>
2023-08-15 23:42:06 +0800

    Section 04 if statement

---
+++ 04-control-logic-and-flow/06-if-statement/app.js
@@ -0,0 +1,19 @@
+// Example 1
+if (1 !== 1) {
+    console.log("It's true!");      // This code will not execute
+}
+
+// Example 2
+let rating = 3;
+
+if (rating === 3) {
+    console.log("You are a superstar!");
+}
+
+
+// Example 2
+let num = 37;
+
+if (num % 2 !== 0) {
+    console.log("Odd number");
+} 
