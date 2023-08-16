ce00794e76f61464b60244cd085eb5ed6bfe0343
benj-lazaro <benjie.work@gmail.com>
2023-08-16 22:57:42 +0800

    Section 04 else-if initial commit

---
+++ 04-control-logic-and-flow/07-else-if/app.js
@@ -0,0 +1,15 @@
+// Performance Review
+// 3 = superstar
+// 2 = meets expectations
+// 1 = needs improvement
+// anything else = you're fired
+
+let rating = 1;
+
+if (rating === 3) {
+    console.log("You are a superstar!");
+} else if (rating === 2) {
+    console.log("Meets expectations");
+} else if (rating === 1) {
+    console.log("Needs improvement");
+}
