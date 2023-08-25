d84562814387ed8fd2804b1c9cdf2d87e94a7be0
benj-lazaro <benjie.work@gmail.com>
2023-08-25 19:59:06 +0800

    Section 04 ternary operator initial commit

---
+++ 04-control-logic-and-flow/16-ternary-operator/app.js
@@ -0,0 +1,11 @@
+// Ternary Operator
+
+// Example #1
+let number = 7;
+number === 7 ? console.log("Lucky!") : console.log("Bad!");
+
+
+// Example #2
+let userStatus = "offline";
+let color = userStatus === "offline" ? "Red" : "Green";
+console.log(`User Status: ${color}`);
