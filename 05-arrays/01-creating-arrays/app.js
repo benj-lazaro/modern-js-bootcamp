218f32a95d589f935454a766910b5aa85abde97f
benj-lazaro <benjie.work@gmail.com>
2023-08-26 20:23:29 +0800

    Section 05 creating arrays initial commit

---
+++ 05-arrays/01-creating-arrays/app.js
@@ -0,0 +1,10 @@
+// Array Creation Demo
+
+// Array of strings
+let shoppingList = ["cereal", "cheese", "ice"];
+
+// Array of numbers
+let lottoNumbers = [19, 22, 56, 12, 51];
+
+// Array of mixed values
+let myCollection = [12, "dog", true, null, NaN];
