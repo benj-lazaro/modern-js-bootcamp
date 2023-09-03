3afb7cbc926f881ad6c37037c633c852a5635136
benj-lazaro <benjie.work@gmail.com>
2023-09-03 17:19:19 +0800

    Section 06 create object literal initial commit

---
+++ 06-objects/02-create-object-literals/app.js
@@ -0,0 +1,19 @@
+// Object Literal Creation Demo
+
+const fitbitData = {
+    totalSteps: 308727,
+    totalMiles: 211.7,
+    avgCalorieBurn: 5755,
+    workoutThisWeek: '5 of 7',
+    avgGoodSleep: '2:13', 
+};
+
+console.log("Content of the fitbitData object...");
+console.log(fitbitData);
+
+console.log("Accessing specific properties of the fitbitData object...")
+console.log(`totalSteps: ${fitbitData.totalSteps}`);
+console.log(`totalMiles: ${fitbitData.totalMiles}`);
+console.log(`avgCalorieButn: ${fitbitData.avgCalorieBurn}`);
+console.log(`workoutThisWeek: ${fitbitData.workoutThisWeek}`);
+console.log(`avgGoodSleep: ${fitbitData.avgGoodSleep}`);
