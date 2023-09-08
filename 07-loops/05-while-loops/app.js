7a69ad0a63c7d4a4796dfb12e1c6ddf18728c614
benj-lazaro <benjie.work@gmail.com>
2023-09-08 18:38:08 +0800

    Section 07 while loops initial commit

---
+++ 07-loops/05-while-loops/app.js
@@ -0,0 +1,13 @@
+// While Loops Demo
+
+// Iterate numbers from 0 to 5 using a for loop
+for (let index = 0; index <= 5; index++) {
+    console.log(`For loop ${index}`);
+}
+
+// Iterate numbers from 0 to 5 using a while loop
+let counter = 0;
+while (counter <= 5) {
+    console.log(`While loop ${counter}`);
+    counter ++;
+}
