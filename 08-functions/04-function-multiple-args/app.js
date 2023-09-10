dbda85d088f659bd80d33b9a7a32bee5dc180c32
benj-lazaro <benjie.work@gmail.com>
2023-09-10 18:06:17 +0800

    Section 08 function w/ mutiple args initial commit

---
+++ 08-functions/04-function-multiple-args/app.js
@@ -0,0 +1,24 @@
+// Function With Multiple Arguments
+
+// Function with a single parameter
+function square(num) {
+    console.log(num * num);
+}
+
+// Function with multiple parameters
+function sum(x, y) {
+    console.log(x + y);
+}
+
+function divide(x, y) {
+    console.log(x / y);
+}
+
+console.log("Calling square(2)");
+square(2);
+
+console.log("Calling sum(4, 5)");
+sum(4, 5);
+
+console.log("Calling divide(10, 2)")
+divide(10, 2)
