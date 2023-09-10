598b00a586a693d67b6cc1ed5f6d744a7ddc9306
benj-lazaro <benjie.work@gmail.com>
2023-09-10 18:42:46 +0800

    Section 08 return statement initial commit

---
+++ 08-functions/05-return-statement/app.js
@@ -0,0 +1,9 @@
+// Return Statement Demo
+
+function add(x, y) {
+    return x + y;       // Evaluates & returns a single value (i.e. the sum of two numbers)
+}
+
+const sum = add(10, 10);
+
+console.log(`The sum of 10 and 10 is ${add(10, 10)}.`);
