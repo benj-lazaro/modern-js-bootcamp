9e7a8dff0088d971a03be1e182ea9d534a5c8086
benj-lazaro <benjie.work@gmail.com>
2023-09-30 16:51:57 +0800

    Section 12 into to keyword this initial commit

---
+++ 12-object-methods-this/05-intro-keyword-this/app.js
@@ -0,0 +1,14 @@
+// Introduction To Keyword This
+
+// On a function declaration
+function sayHi() {
+  console.log("Hi!");
+  console.log(this); // Returns the 'window' object; global scople of the browser
+}
+sayHi();
+
+// On a regular function express
+const greet = function () {
+  console.log(this); // Returns the same 'window' objecct
+};
+greet();
