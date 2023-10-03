a537b7b7f66909307784275bdc3c50e92b6e0b35
benj-lazaro <benjie.work@gmail.com>
2023-10-03 20:05:48 +0800

    Section 12 annoyomatic demo content refactor

--- 12-object-methods-this/08-annoyomatic-demo/app.js
+++ 12-object-methods-this/08-annoyomatic-demo/app.js
@@ -1,7 +1,6 @@
 // Annoyomatic Demo
 
-// Run code on the browser's JavaScript console
-
+// 'this' inside an object literal
 const annoyer = {
   phrases: [
     "literally",
@@ -12,23 +11,27 @@ const annoyer = {
     "Can't Stop, Won't Stop",
   ],
   pickPhrase() {
-    // Randomly pick a phrase from the object literal's property 'phrases'
-    const { phrases } = this;
+    const { phrases } = this; // 'this' current execution scope = object literal 'annoyer'
     const index = Math.floor(Math.random() * phrases.length);
     return phrases[index];
   },
+  // start() is a method of object literal 'annoyer'; current execution scope = object literal 'annoyer'
   start() {
-    // Instantaneously add the '.timerId' as a property to the object literal 'annoyer'
-    // The window.SetInterval() returns an ID no. that will be used to stop the current interval
+    // setInterval() is a function of browser's object 'window'; current execution scope = 'window'
+    // setInterval()'s receives an Arrow Function as the 1st argument
+    // Arrow function's 'this' value is altered by the .start() method's current exection scope
+    // setInterval() returns an ID that is saved in a property named timerId of the object literal 'annoyer'
     this.timerId = setInterval(() => {
-      // Using an arrow function, the value of 'this' references to the object literal 'annoyer'
-      console.log(this.pickPhrase());
+      // 'this' of the timerId property's current execution scope = object literal 'annoyer'
+      console.log(this.pickPhrase()); // 'this' current execution scope = object literal 'annoyer'
     }, 3000);
   },
+  // stop() is a method of object literal 'annoyer'; current execution scope = object literal 'annoyer'
   stop() {
-    // window.clearInterval() accesses the '.timerId' property of the object literal 'annoyer'
-    // Stops the running interval
-    clearInterval(this.timerId);
+    // clearInterval() is a function of browser's object 'window'; current execution scope = 'window'
+    // clearInterval() requires an ID to stop the ongoing internval set by setInterval()
+    // clearInterval() accesses the ID from the object literal's property timerId to stop the interval
+    clearInterval(this.timerId); // 'this' current execution scope = object literal 'annoyer'
     console.log("Phew! Thank heavens that is over.");
   },
 };
