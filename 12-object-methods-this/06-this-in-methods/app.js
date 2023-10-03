37dc2f6fe0afd6227084dad2ce7f3998d791470e
benj-lazaro <benjie.work@gmail.com>
2023-10-03 14:28:54 +0800

    Section 12 this in methods content refactor

--- 12-object-methods-this/06-this-in-methods/app.js
+++ 12-object-methods-this/06-this-in-methods/app.js
@@ -1,27 +1,29 @@
 // Using This In Methods Demo
 
+// 'this' inside a regular function
 function sayHi() {
   console.log("Hi");
-  console.log(this);
+  console.log(this); // 'this' references the browser's object 'window' (current execution scope)
 }
 
-sayHi(); // Prints the string & returns a reference to the global 'window' object of the browser
-
+// 'this' inside an object literal
 const person = {
   first: "Cherilyn",
   last: "Sarkisian",
   nickName: "Cher",
   fullName() {
-    // Destructure object's properties to be referenced by keyword 'this'
-    // Prevents repeatedly using 'this' keyword explicitly on each property
-    // console.log(`${this.first} ${this.last} a.k.a ${this.nickName}`);
-    const { first, last, nickName } = this;
+    // Destructure the object literal's properties to be referenced by 'this'
+    const { first, last, nickName } = this; // 'this' references the object literal ''person'
     return `${first} ${last} AKA ${nickName}`;
   },
   printBio() {
-    const fullName = this.fullName(); // Calls the object's .fullName() method
+    const fullName = this.fullName(); // 'this' references the object literal ''person'
     console.log(`${fullName} is a person!`);
   },
 };
 
-console.log(person.printBio()); // Calls the object's .printBio() method
+// Invoke regular function sayHi()
+sayHi();
+
+// Invoke the .printBio() method of the object literal 'person'
+person.printBio();
