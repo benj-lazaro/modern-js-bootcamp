06f7e40d004bed91ed435790dd21096c26c21b62
benj-lazaro <benjie.work@gmail.com>
2023-10-03 16:41:30 +0800

    Section 12 this invocation context refactor

--- 12-object-methods-this/07-this-invocation-context/app.js
+++ 12-object-methods-this/07-this-invocation-context/app.js
@@ -1,6 +1,6 @@
 // This Invocation Context Demo
 
-// Keyword this inside an object literal's methods
+// 'this' inside an object literal
 const person = {
   first: "Cherilyn",
   last: "Sarkisian",
@@ -10,29 +10,33 @@ const person = {
     return `${first} ${last} AKA ${nickName}`;
   },
   printBio() {
-    console.log(this); // Displays the current value of 'this'
-    const fullName = this.fullName();
+    console.log(this);
+    const fullName = this.fullName(); // Current execution scope = object literal itself
     console.log(`${fullName} is a person!`);
   },
   laugh: () => {
-    console.log(this);
+    // An arrow function is used to define the method
+    console.log(this); // Current execution scope = browser's object 'window'
     console.log(`${this.nickName} says "Ha Ha Ha Ha"`);
   },
 };
 
-// Invoke the .printBio() method of the object literal 'person' normally
-// 'this' references its parent object literal 'person'
+// Invoke the .printBio() method of the object literal 'person'
+// Invocation context = <object_literal>.<method_name>()
+// Current execution scope = object literal itself
 console.log(person.printBio());
 
-// Invoke the arrow function (method) .laugh() of the object literal 'person'
-// 'this' references to the global execution scope object 'window'
-// Returns 'undefined says  "Ha Ha Ha Ha"'
+// Invoke the .laugh() method of the object literal 'person'; DEFINED as an Arrow Function
+// Invocation context = <object_literal>.<method_name>()
+// Current execution scope = the browser's global scope object 'window'
+// Returns 'undefined says  "Ha Ha Ha Ha"' since .nickName is NOT a property of the object 'window'
 console.log(person.laugh());
 
 // Assign the .printBio() method of the object literal 'person' to a variable as reference
 const printBio2 = person.printBio;
 
-// Invoke the variable as a stand-alone function
-// 'this' now references the global execution scope object 'window'
-// Triggers a TypeError since 'this'.fullName() is NOT a function of the global object 'window'
+// Invoke the variable as a regular function
+// Invocation context = <function_name>()
+// Current execution scope = the browser's global scope object 'window'
+// Returns a TypeError since .fullName() is NOT a method of the global scope object 'window'
 printBio2();
