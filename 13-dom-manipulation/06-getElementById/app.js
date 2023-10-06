52cc7822c51ac9560ac8f515726fc93a96b0dd85
benj-lazaro <benjie.work@gmail.com>
2023-10-06 20:20:21 +0800

    Section 13 .getElementById() initial commit

---
+++ 13-dom-manipulation/06-getElementById/app.js
@@ -0,0 +1,12 @@
+// .getElementById() Demo
+
+// Select an element object based on its ID attribute value
+const img = document.getElementById("starship-photo");
+// Display properties of retrieved element object
+console.dir(img);
+// Display the associated HTML element (tag & content)
+console.log(img);
+
+const p = document.getElementById("main");
+console.dir(p);
+console.log(p);
