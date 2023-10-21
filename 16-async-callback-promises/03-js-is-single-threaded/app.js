6ed3408948fae2009dfb99e3c7e9ff991f965626
benj-lazaro <benjie.work@gmail.com>
2023-10-21 16:27:15 +0800

    Section 16 javascript is single threaded refactor

--- 16-async-callback-promises/03-js-is-single-threaded/app.js
+++ 16-async-callback-promises/03-js-is-single-threaded/app.js
@@ -2,5 +2,5 @@
 
 // JavaScript interprets one line of code at a time
 console.log("I happen first!");
-alert("Hi there!");
+alert("Hi there!"); // JavaScript waits for the 'ok' button to be clicked before proceeding
 console.log("I happen second!");
