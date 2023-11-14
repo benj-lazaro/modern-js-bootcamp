6381abb3323fbe9ff8a13e42d5ae46b12ab48883
benj-lazaro <benjie.work@gmail.com>
2023-11-14 17:27:50 +0800

    Section 20 animation secrets initial commit

---
+++ 20-drawing-animations/20-animation-secret/app.js
@@ -0,0 +1,19 @@
+// The Secrets To The Animation Timer Web App Project
+
+// Target DOM elements
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+// Instantiate Timer class into an object with optional callback functions
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer started");
+  },
+  onTick() {
+    console.log("Timer just ticked down");
+  },
+  onComplete() {
+    console.log("Timer is completed");
+  },
+});
