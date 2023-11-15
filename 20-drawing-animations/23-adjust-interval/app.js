8903a93bf0cb28d1889d6eca9c7db4137c001c9a
benj-lazaro <benjie.work@gmail.com>
2023-11-16 02:12:16 +0800

    Section 20 adjust interval initial commit

---
+++ 20-drawing-animations/23-adjust-interval/app.js
@@ -0,0 +1,35 @@
+// Adjusting By An Even Interval Timer Web App Project
+
+// Target DOM elements
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+// Target DOM element of the <circle>
+const circle = document.querySelector("circle");
+
+// Retrieve <circle> attribute 'r' & compute the perimeter value
+const perimeter = circle.getAttribute("r") * 2 * Math.PI;
+
+// Set the 'stroke-dasharray' attribute & value
+circle.setAttribute("stroke-dasharray", perimeter);
+
+// Initialize variable for the duration of the timer
+let duration;
+
+// Instantiate Timer class into an object with optional callback functions
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart(totalDuration) {
+    console.log("Timer started");
+    duration = totalDuration;
+  },
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      "stroke-dashoffset",
+      (perimeter * timeRemaining) / duration - perimeter
+    );
+  },
+  onComplete() {
+    console.log("Timer is completed");
+  },
+});
