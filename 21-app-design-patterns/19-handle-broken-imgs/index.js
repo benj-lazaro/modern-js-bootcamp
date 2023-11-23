8418c97e78120f7fda861977bb8afe65895f88ff
benj-lazaro <benjie.work@gmail.com>
2023-11-23 15:24:54 +0800

    Section 21 handle broken imgs initial commit

---
+++ 21-app-design-patterns/19-handle-broken-imgs/index.js
@@ -0,0 +1,72 @@
+// Movie Fight Web Application
+
+// Request data from OMDb API
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "eeec100e",
+      s: searchTerm,
+    },
+  });
+
+  // Check if the fetched data has an 'Error' property present & return an empty array
+  if (response.data.Error) {
+    return [];
+  }
+
+  // Return the fetched array of movies
+  return response.data.Search;
+};
+
+// Target the root element of the autocomplete widget
+const root = document.querySelector(".autocomplete");
+
+// Generate HTML code for the root element
+root.innerHTML = `
+  <label><b>Search For A Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results">
+      </div>
+    </div>
+  </div>
+`;
+
+// Target elements from the generated innerHTML value of the root variable
+const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
+
+// Refactor onInput as an async function similar to that of fetchData()
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+
+  // Clears out previous dropdown menu list stored in the resultsWrapper
+  resultsWrapper.innerHTML = "";
+
+  // Enable dropdown in preparation for adding each movie entry as a dropdown menu item
+  dropdown.classList.add("is-active");
+
+  // Iterate through the returned array (i.e. movies)
+  for (let movie of movies) {
+    // Retrieve movie poster & title; write each to their corresponding DOM element
+    const option = document.createElement("a");
+
+    // Check movie poster for broken image (e.g. 'Poster': 'N/A')
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+
+    // Add movie as an item in the dropdown menu
+    option.classList.add("dropdown-item");
+    option.innerHTML = `
+      <img src="${imgSrc}" />
+      ${movie.Title}
+    `;
+
+    // Append the each movie entry as a child element to resultsWrapper
+    resultsWrapper.appendChild(option);
+  }
+};
+
+// Calls debounce() for every keypress then calls onInput() 500ms after the last keypress
+input.addEventListener("input", debounce(onInput, 500));
