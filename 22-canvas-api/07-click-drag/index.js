8d215b63e061f6d549790b0378431283aca38702
benj-lazaro <benjie.work@gmail.com>
2023-12-12 17:16:14 +0800

    Section 22 click drag initial commit

---
+++ 22-canvas-api/07-click-drag/index.js
@@ -0,0 +1,43 @@
+// Maze Game Project
+
+const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } =
+  Matter;
+
+const engine = Engine.create();
+
+const { world } = engine;
+
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600,
+  },
+});
+
+Render.run(render);
+
+Runner.run(Runner.create(), engine);
+
+// Create a mouse constaint then add it to the World object
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+  })
+);
+
+// Draw walls along the borders of the World object (top, bottom, left & right)
+const walls = [
+  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+];
+
+// Add shapes to the World object
+World.add(world, walls);
+
+// Crash dummy rectangle
+World.add(world, Bodies.rectangle(200, 200, 40, 40));
