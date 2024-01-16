d4c25a93edb1061894d6317cbeb4e4b610cc16c4
benj-lazaro <benjie.work@gmail.com>
2024-01-16 16:08:26 +0800

    Section 23 decode the message initial commit

---
+++ 23-secret-message-app/09-decoding-the-message/index.js
@@ -0,0 +1,22 @@
+// Check URL and destructure the hash part from window.location
+const { hash } = window.location;
+
+// Replace the hash part with an empty string then & decode the hash on the JavaScript console
+console.log(atob(hash.replace("#", "")));
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  // Prevent default of sending submitted form data to a remote backend server
+  event.preventDefault();
+
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
+
+  // Encode the message using Base64 encoding
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+
+  // Generate a URL and append the encoded message
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+});
