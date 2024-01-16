6a39a510e047ee4a1bd8d7d7f73ec5a7c7663ed9
benj-lazaro <benjie.work@gmail.com>
2024-01-16 13:24:58 +0800

    Section 23 toggle element visibility initial commit

---
+++ 23-secret-message-app/08-toggle-element-visibility/index.js
@@ -0,0 +1,16 @@
+document.querySelector("form").addEventListener("submit", (event) => {
+  // Prevent default of sending submitted form data to a remote backend server
+  event.preventDefault();
+
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
+
+  // Encode the message using Base64 encoding
+  const input = document.querySelector("#message-input");
+  const encrypted = btoa(input.value);
+
+  // Generate a URL and append the encoded message
+  const linkInput = document.querySelector("#link-input");
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+});
