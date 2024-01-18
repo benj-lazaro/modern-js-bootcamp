7f331a511f528a9920ab2024ef915d5eba71a331
benj-lazaro <benjie.work@gmail.com>
2024-01-18 15:58:13 +0800

    Section 24 work with modules initial commit

---
+++ 24-nodejs-cmdline-tools/03-work-with-modules/index.js
@@ -0,0 +1,5 @@
+// Import 'myscript.js' & assign the export value into the variable message
+const message = require("./myscript.js");
+
+// Display the content of the variable message
+console.log(message);
