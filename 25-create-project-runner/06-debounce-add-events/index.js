c5fa8980f6b34653401aeb9795e979443b7822be
benj-lazaro <benjie.work@gmail.com>
2024-01-31 16:42:12 +0800

    Section 25 debounce add events initial commit

---
+++ 25-create-project-runner/06-debounce-add-events/index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+const debounce = require("lodash.debounce");
+const chokidar = require("chokidar");
+
+// Store debounced function for chokidar's "add" event with 100ms delay in variable 'start'
+const start = debounce(() => {
+  console.log("Starting user's program");
+}, 100);
+
+// Watch the cwd for file creation, change or deletion events & call corresponding callback
+chokidar
+  .watch(".")
+  .on("add", start)
+  .on("change", () => {
+    console.log("File Changed");
+  })
+  .on("unlink", () => {
+    console.log("File Deleted");
+  });
