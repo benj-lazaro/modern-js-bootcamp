9d4a9d9cc8fec0ad193835ad84e662d44f5946ea
benj-lazaro <benjie.work@gmail.com>
2024-02-24 17:01:24 +0800

    Section 28 testing full flow initial commit

---
+++ 28-prod-grade-authentication/10-testing-full-flow/index.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const usersRepo = require("./repositories/users");
+
+const app = express();
+
+// To have the middleware function available to every route handler
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Call cookieSession() and pass a configuration object
+// Used as an encryption key to secure information stored inside the cookie
+app.use(
+  cookieSession({
+    keys: ["3ie98eri80929weudd1231287f"],
+  })
+);
+
+// Root route GET request handler
+app.get("/signup", (req, res) => {
+  res.send(`
+    <div>
+      Your ID is: ${req.session.userId}
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+// Root route POST request handler
+app.post("/signup", async (req, res) => {
+  // Destructure request body & assign values to corresponding variables
+  const { email, password, passwordConfirmation } = req.body;
+
+  // Check for existing account based on registered email address
+  const existingUser = await usersRepo.getOneBy({ email });
+
+  if (existingUser) {
+    return res.send("Email in use");
+  }
+
+  // Check if password & password configuration are the same
+  if (password !== passwordConfirmation) {
+    return res.send("Passwords must   match");
+  }
+
+  // Otherwise, create a user record in the UserRepository (users.json)
+  const user = await usersRepo.create({ email, password });
+
+  // Store the ID inside the user's cookie
+  req.session.userId = user.id;
+
+  res.send("Account Created!!!");
+});
+
+// Sign-out route GET request handler
+app.get("/signout", (req, res) => {
+  // Forget information stored inside the cookie
+  req.session = null;
+  res.send("You are logged out");
+});
+
+// Sign-in route GET request handler
+app.get("/signin", (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <button>Sign In</button>
+      </form>
+    </div>
+  `);
+});
+
+// Sign-in route POST request handler
+app.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  // Retrieve record based on provided credentails
+  const user = await usersRepo.getOneBy({ email });
+
+  // Check if email address exists in the records
+  if (!user) {
+    return res.send("Email not found.");
+  }
+
+  // Check if password matches with the password in the record
+  const validPassword = await usersRepo.comparePasswords(
+    user.password,
+    password
+  );
+
+  if (!validPassword) {
+    return res.send("Invalid password");
+  }
+
+  // Otherwise, login the user
+  req.session.userId = user.id;
+  res.send(`
+    <div>You are signed in!</div>
+    <div>Your ID is: ${req.session.userId}</div>
+  `);
+});
+
+// Listening port
+app.listen(3000, () => {
+  console.log("Listening...");
+});
