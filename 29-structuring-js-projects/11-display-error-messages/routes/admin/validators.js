6448be9d413895e4190d373f03f6b909266ea8bb
benj-lazaro <benjie.work@gmail.com>
2024-03-04 21:53:55 +0800

    Section 29 display error messages initial commit

---
+++ 29-structuring-js-projects/11-display-error-messages/routes/admin/validators.js
@@ -0,0 +1,33 @@
+// Hook-up the check function of the express-validator library
+const { check } = require("express-validator");
+
+// Hook-up the users' repository codebase
+const usersRepo = require("../../repositories/users");
+
+// Export validation chain
+module.exports = {
+  requireEmail: check("email")
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Must be a valid email")
+    .custom(async (email) => {
+      const existingUser = await usersRepo.getOneBy({ email });
+      if (existingUser) {
+        throw new Error("Email in use");
+      }
+    }),
+  requirePassword: check("password")
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage("Must be between 4 and 20 characters"),
+  requirePasswordConfirmration: check("passwordConfirmation")
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage("Must be between 4 and 20 characters")
+    .custom((passwordConfirmation, { req }) => {
+      if (passwordConfirmation !== req.body.password) {
+        throw new Error("Passwords must match");
+      }
+    }),
+};
